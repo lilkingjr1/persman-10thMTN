f2861fd9bc846cc07d9f39b35074f5fa2e10d7dd
Red-Thirten <rehlmgaming@gmail.com>
2020-05-17 01:26:45 -0700

    Calendar Update
    Added more event types to the calendar.

--- routes/calendar.js
+++ routes/calendar.js
@@ -33,11 +33,26 @@ router.get("/calendar/event", isLoggedIn, function(req,res){
 router.post("/calendar/event", isLoggedIn, function(req,res){
     if(req.user.role.num < 2) return res.redirect("/");
     let event = {};
-    if(req.body.eventtype === "Basic Training") {
-        event = {type: "Basic Training", color : "#28a745"};
-    } else if(req.body.eventtype === "Operation") {
-        event = {type: "Operation", color : "#007bff"};
-    }
+	switch(req.body.eventtype) {
+		case "Basic Combat Training":
+			event = {type: "Basic Combat Training", color : "#007bff"};
+			break;
+		case "Qualification":
+			event = {type: "Qualification", color : "#663399"};
+			break;
+		case "Training/FTX":
+			event = {type: "Training/FTX", color : "#33993C"};
+			break;
+		case "Deployment":
+			event = {type: "Deployment", color : "#993333"};
+			break;
+		case "Staff Meeting":
+			event = {type: "Staff Meeting", color : "#E6A118"};
+			break;
+		default:
+			event = {type: "", color : "#000000"};
+			break;
+	}
     const body = req.body;
     const newEvent = new Calendar({
         title: body.eventname,
